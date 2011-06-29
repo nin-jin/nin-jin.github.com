3849b4c5b45b7c69557e0f66463d032d18499142
nin-jin <nin-jin@ya.ru>
2011-06-29 08:56:10 +0400

    / -> web-componets compiled resources

--- jam/schedule/jam+schedule.js
+++
@@ -1,7 +0,0 @@
-with( $jam )
-$define( '$schedule', function( timeout, proc ){
-    var timerID= $glob().top.setTimeout( proc, timeout )
-    return function( ){
-        $glob().top.clearTimeout( timerID )
-    }
-})
