4041af60c2427e4e64f88e338ca188972cfcd4ea
nin-jin <nin-jin@ya.ru>
2014-10-13 22:45:37 +0400

    dart vs typescript

---
+++ ray/dart/dart_raytracerdemo.dart
@@ -0,0 +1,13 @@
+// Converted to Dart by Antonino Porcino, Aug 22, 2014
+// nino.porcino@gmail.com
+
+// switch the following comment to alternate between the two versions:
+
+//import 'SimpleRaytracer.simd.dart';
+import 'SimpleRaytracer.const.dart';
+//import 'SimpleRaytracer.dart';
+
+void main()
+{
+  RayTracer.Main();
+}
